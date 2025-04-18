1a2e13120011985cf47e008fb750d245ede61e91
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-04-19 06:58:31 +0700

    feat: enhance Navbar with social links and navigation options

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { Socials } from '@/constants'
 
 const Navbar = () => {
     return (
@@ -16,8 +17,26 @@ const Navbar = () => {
                         Abicode.
                     </span>
                 </a>
-            </div>
 
+                <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
+                    <div className='flex items-center justify-evenly w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-white'>
+                        <a href="#about-me" className='cursor-pointer'>About me</a>
+                        <a href="#skills" className='cursor-pointer'>Skills</a>
+                        <a href="#projects" className='cursor-pointer'>Projects</a>
+                    </div>
+                </div>
+                
+                <div className='flex flex-row gap-5'>
+                    {Socials.map((social) => (
+                        <Image
+                        src={social.src}
+                        alt={social.name}
+                        key={social.name}
+                        width={24}
+                        height={24}/>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
