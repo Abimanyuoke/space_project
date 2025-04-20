e864380a4e83daec725ea59e875f80a0c14966ac
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-04-20 07:51:44 +0700

    Added Projects component to Home page

---
+++ components/main/Projects.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ProjectCard from '../sub/ProjectCard'
+
+const Projects = () => {
+  return (
+    <div className='flex flex-col items-center justify-center py-20'>
+        <h1 className='text-[40px] font-semibold  text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
+            My Projects
+        </h1>
+        <div className='h-full flex flex-col md:flex-row gap-10 px-10'>
+            <ProjectCard
+                title='Modern Next.Js Portfolio'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius labore facere iusto animi minus.'
+                src='/NextWebsite.png'/>
+            <ProjectCard
+                title='Interactive Website Cards'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius labore facere iusto animi minus.'
+                src='/CardImage.png'/>
+            <ProjectCard
+                title='Space Themed Website'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius labore facere iusto animi minus.'
+                src='/SpaceWebsite.png'/>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
