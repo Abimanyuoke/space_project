c52c566db54ce156c7c6117297068c8b86b53e5e
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-04-20 08:15:25 +0700

    feat: Added Footer component and updated Hero and Projects components with IDs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,6 +22,7 @@ export default function RootLayout({
         <StarsCanvas/>
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
